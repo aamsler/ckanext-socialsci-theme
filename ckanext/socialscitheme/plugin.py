aae5e4d4d58a9a7d48d62b7a212f6f5a431967d3
Rachel Knowler <rachel.knowler@liip.ch>
2015-03-30 11:11:40 +0200

    Adding query functions.

--- ckanext/socialscitheme/plugin.py
+++ ckanext/socialscitheme/plugin.py
@@ -4,6 +4,42 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as tk
 
 
+def get_biggest_groups(n):
+    """Returns the n biggest groups, to display on start page."""
+    user = tk.get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'user': user['name']}
+    data_dict = {
+        'all_fields': True,
+    }
+    groups = tk.get_action('group_list')(context, data_dict)
+    if len(groups) > n:
+        return sorted(groups, key=lambda group: group['packages'])[-1:-(n+1):-1]
+    else:
+        return sorted(groups, key=lambda group: group['packages'])[::-1]
+
+
+def get_newest_groups():
+    """Returns the n most recently updated groups, to display on start page."""
+    user = tk.get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'user': user['name']}
+    data_dict = {
+        'all_fields': True,
+    }
+    groups = tk.get_action('group_list')(context, data_dict)
+    if len(groups) > n:
+        return sorted(groups, key=lambda group: group['packages'])[-1:-(n+1):-1]
+    else:
+        return sorted(groups, key=lambda group: group['packages'])[::-1]
+
+
+def get_newest_datasets():
+    pass
+
+
+def get_most_used_datasets():
+    pass
+
+
 class SocialSciThemePlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
 
     plugins.implements(plugins.IConfigurer, inherit=False)
@@ -26,3 +62,7 @@ class SocialSciThemePlugin(plugins.SingletonPlugin, tk.DefaultDatasetForm):
         # registers itself as the default (above).
         return []
 
+    def get_helpers(self):
+        return {
+            'get_biggest_groups': get_biggest_groups
+        }
